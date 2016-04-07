7c895eec8469e5f8c61293b4b791e744f1704895
JJ Lueck <jlueck@tockhq.com>
2016-04-07 16:30:39 -0500

    Updates to the scripts which now seem to work correctly.

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+LABEL=$(date +"%Y-%m-%dT%H-%M-%S")
+
 # Tag the current release in case of rollback
 gcloud docker pull gcr.io/elizabeth-restaurant/web:latest
 docker tag -f gcr.io/elizabeth-restaurant/web:latest gcr.io/elizabeth-restaurant/web:rollback
@@ -9,7 +11,9 @@ gcloud docker push gcr.io/elizabeth-restaurant/web:rollback
 
 # Build the current release
 docker build -t gcr.io/elizabeth-restaurant/web:latest .
+docker tag -f gcr.io/elizabeth-restaurant/web:latest gcr.io/elizabeth-restaurant/web:${LABEL}
 gcloud docker push gcr.io/elizabeth-restaurant/web:latest
+gcloud docker push gcr.io/elizabeth-restaurant/web:${LABEL}
 
 # Update the current release
 gcloud config set container/cluster web
@@ -23,4 +27,4 @@ kubectl config use-context gke_elizabeth-restaurant_us-central1-f_web
 # Using replication controllers
 #kubectl create -f web.rc.yaml
 #kubectl expose rc web --port=80 --target-port=8080 --type="LoadBalancer"
-kubectl rolling-update web --update-period=10s -f web.rc.yaml
+kubectl rolling-update web --update-period=10s --image=gcr.io/elizabeth-restaurant/web:${LABEL}
--- scripts/rollback_image.sh
+++ scripts/rollback_image.sh
@@ -5,3 +5,5 @@ set -x
 gcloud docker pull gcr.io/elizabeth-restaurant/web:rollback
 docker tag -f gcr.io/elizabeth-restaurant/web:rollback gcr.io/elizabeth-restaurant/web:latest
 gcloud docker push gcr.io/elizabeth-restaurant/web:latest
+
+kubectl rolling-update web --update-period=10s --image=gcr.io/elizabeth-restaurant/web:latest
